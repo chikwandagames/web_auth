c94ae1f983be7f4313485d75ec3b051754446f4b
chikwandagames <chikwandagames@gmail.com>
2021-03-31 17:17:54 +0100

    Marshal

--- main.go
+++ main.go
@@ -1,5 +1,36 @@
 package main
 
-func main() {
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+)
 
+// Marshalling
+// Tacking go code and turning into JSON
+
+type person struct {
+	// Capital "F" for exporting
+	First string
+}
+
+func main() {
+	p1 := person{
+		First: "John",
+	}
+
+	p2 := person{
+		First: "Trev",
+	}
+
+	xp := []person{p1, p2}
+
+	bs, err := json.Marshal(xp)
+
+	if err != nil {
+		log.Panic(err)
+	}
+
+	fmt.Printf("%T \n", bs)
+	fmt.Println(string(bs))
 }
